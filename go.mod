ab9b1bc2c031c56f27743608692e72d88ff4f11b
Marvin <marvin@test.com>
2024-01-09 12:55:31 +0000

    refactored the routing code using chi library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mperalle/cs50-final-project
 
 go 1.21.4
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // declaration of the handler function of type http.HandlerFunc to pass it in http.HandleFunc
@@ -21,26 +23,29 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:marvin@cs50.com\">marvin@cs50.com</a>.</p>")
 }
 
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "404 page not found", http.StatusNotFound)
-}
-
-// declaration of a basic router
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		NotFound(w, r)
-	}
-
+// declaration of handler function for faq page
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	//set header content-type to html
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//writing the HTML response
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li> <b>Is this application free to use ?</b> Yes it is completly free to use !</li>
+		<li> <b>How can I get in touch with you ?</b> You can contact me via the email in the contact page</li>
+	</ul>`)
 }
 
 func main() {
+	//register handler functions to a new Chi Routeur
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "404 Error Page not found", http.StatusNotFound)
+	})
 	//start the server on port :3000
 	fmt.Println("Starting the server on port :3000 ...")
-	//convert the pathHandler into HandlerFunc type in order to implement Handler interface and pass it to ListenAndServe
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	//pass the routeur to ListenAndServe
+	http.ListenAndServe(":3000", r)
 }
